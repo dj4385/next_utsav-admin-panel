5c6a62a83b06f36574a661dccaa7a0e93543b6b5
dheeraj <dj4385@gmail.com>
2025-03-31 16:59:04 +0530

    contact page inprogress

--- app/(dashboard)/contact/_components/ContactFooter/ContactFooter.tsx
+++ app/(dashboard)/contact/_components/ContactFooter/ContactFooter.tsx
@@ -1,10 +1,23 @@
 "use client"
 
-import { IContactFooter, IContactFooterData } from "@/app/types/components/Contact";
+import { IContactFooter, IContactFooterData, IContactFooterList } from "@/app/types/components/Contact";
 import FileUploader from "@/components/FileUploader/FileUploader";
 import { Input } from "@/components/ui/input";
-import { Book } from "lucide-react";
+import { useAppDispatch, useAppSelector } from "@/lib/store";
+import { Book, CirclePlus, Edit2, Trash2 } from "lucide-react";
 import { ChangeEvent, useEffect } from "react";
+
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
+import Image from "next/image";
+import { Separator } from "@/components/ui/separator";
+import { Button } from "@/components/ui/button";
 
 const ContactFooter = ({
     contactFooterData,
@@ -12,6 +25,9 @@ const ContactFooter = ({
     setContactFooterList,
     setContactFooterData
 }: IContactFooter) => {
+
+    const dispatch = useAppDispatch();
+    // const {} = useAppSelector((state) => state)
 
     const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
         const { name, value } = e.target;
@@ -29,6 +45,20 @@ const ContactFooter = ({
         }));
     }
 
+    const openModal = () => {
+        // dispatch(setAboutGalleryModal(true));
+    }
+
+    const editModal = (data: IContactFooterList) => {
+        // dispatch(setAboutGalleryListItem(data))
+        openModal();
+    }
+
+    const deleteData = (id: string) => {
+        const data = contactFooterList.filter((d) => d._id !== id);
+        setContactFooterList(data);
+    }
+
     return (
         <div className="border-[2px] rounded-lg overflow-hidden w-full bg-white">
             <h2 className="flex flex-row gap-2 p-2 bg-purple-700 text-white items-center text-lg font-medium mb-3"> <Book /> Footer</h2>
@@ -41,6 +71,54 @@ const ContactFooter = ({
                     <label className="block text-sm font-medium text-gray-700">Alt Footer Image</label>
                     <Input type="text" placeholder="Enter Alt Footer Image" name="alt_footer_image" onChange={handleChange} value={contactFooterData?.alt_footer_image || ''} className="mt-1 w-full" />
                 </div>
+            </div>
+            <Separator className="mt-4" />
+            <div className="p-2">
+                <div className="flex justify-end items-center mb-2">
+                    <Button variant="secondary" className="bg-purple-600 text-white hover:bg-purple-800" onClick={() => openModal()}>
+                        <CirclePlus /> Add Footer
+                    </Button>
+                </div>
+                <Table className="rounded-md">
+                    <TableHeader>
+                        <TableRow className="bg-purple-600 hover:bg-purple-600 text-white">
+                            <TableHead className="text-white">Image</TableHead>
+                            <TableHead className="text-white">Image Alt</TableHead>
+                            <TableHead className="text-white">Name</TableHead>
+                            <TableHead className="text-white">Address</TableHead>
+                            <TableHead className="text-white">Email</TableHead>
+                            <TableHead className="text-white">Phone</TableHead>
+                            <TableHead className="text-white text-right">Action</TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        {contactFooterList.map((item, index) => (
+                            <TableRow key={index}>
+                                <TableCell>
+                                    {
+                                        item && item.country_image ? <div className="h-[50px] w-[50px]"> <Image src={item.country_image} alt="image" width={50} height={50} className="h-full w-full rounded-md" /> </div> : null
+                                    }
+                                </TableCell>
+                                <TableCell>{item.alt_country_image}</TableCell>
+                                <TableCell>{item.country_name}</TableCell>
+                                <TableCell>{item.country_address}</TableCell>
+                                <TableCell>{item.email}</TableCell>
+                                <TableCell>{item.phone}</TableCell>
+                                <TableCell>
+                                    <div className="flex items-center justify-end gap-2">
+                                        <Button variant="secondary" size="icon" onClick={() => editModal(item)}>
+                                            <Edit2 />
+                                        </Button>
+                                        <Button variant="destructive" size="icon" onClick={() => deleteData(item._id)}>
+                                            <Trash2 />
+                                        </Button>
+                                    </div>
+
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
             </div>
         </div>
     )
--- app/(dashboard)/contact/_components/GlobalPresence/GlobalPresence.tsx
+++ app/(dashboard)/contact/_components/GlobalPresence/GlobalPresence.tsx
@@ -1,10 +1,23 @@
 "use client"
 
-import { IGlobalPresence, IGlobalPresenceData } from "@/app/types/components/Contact";
+import { IGlobalPresence, IGlobalPresenceData, IGlobalPresenceList } from "@/app/types/components/Contact";
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
 import { Textarea } from "@/components/ui/textarea";
-import { Globe2 } from "lucide-react";
+import { CirclePlus, Edit2, Globe2, Trash2 } from "lucide-react";
 import { ChangeEvent, useEffect } from "react";
+
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
+import Image from "next/image";
+import { useAppDispatch, useAppSelector } from "@/lib/store";
 
 const GlobalPresence = ({
     globalPresenceData,
@@ -12,7 +25,9 @@ const GlobalPresence = ({
     setGlobalPresenceData,
     setGlobalPresenceList
 }: IGlobalPresence) => {
-
+    const dispatch = useAppDispatch();
+    // const {} = useAppSelector((state) => state)
+    
     const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
         const { name, value } = e.target;
 
@@ -21,6 +36,21 @@ const GlobalPresence = ({
             [name]: value,
         }));
     }
+
+    const openModal = () => {
+        // dispatch(setAboutGalleryModal(true));
+    }
+
+    const editModal = (data: IGlobalPresenceList) => {
+        // dispatch(setAboutGalleryListItem(data))
+        openModal();
+    }
+
+    const deleteData = (id: string) => {
+        const data = globalPresenceList.filter((d) => d._id !== id);
+        setGlobalPresenceList(data);
+    }
+
 
     return (
         <div className="border-[2px] rounded-lg overflow-hidden w-full bg-white">
@@ -34,6 +64,50 @@ const GlobalPresence = ({
                     <label className="block text-sm font-medium text-gray-700">Global Presence Sub Heading</label>
                     <Textarea placeholder="Enter sub heading here" onChange={handleChange}  name="global_presence_sub_heading" value={globalPresenceData?.global_presence_sub_heading || ''} />
                 </div>
+            </div>
+            <Separator className="mt-4" />
+            <div className="p-2">
+                <div className="flex justify-end items-center mb-2">
+                    <Button variant="secondary" className="bg-purple-600 text-white hover:bg-purple-800" onClick={() => openModal()}>
+                        <CirclePlus /> Add Global Presence
+                    </Button>
+                </div>
+                <Table className="rounded-md">
+                    <TableHeader>
+                        <TableRow className="bg-purple-600 hover:bg-purple-600 text-white">
+                            <TableHead className="text-white">Image</TableHead>
+                            <TableHead className="text-white">Image Alt</TableHead>
+                            <TableHead className="text-white">Name</TableHead>
+                            <TableHead className="text-white">Address</TableHead>
+                            <TableHead className="text-white text-right">Action</TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        {globalPresenceList.map((item, index) => (
+                            <TableRow key={index}>
+                                <TableCell>
+                                    {
+                                        item && item.country_image ? <div className="h-[50px] w-[50px]"> <Image src={item.country_image} alt="image" width={50} height={50} className="h-full w-full rounded-md" /> </div> : null
+                                    }
+                                </TableCell>
+                                <TableCell>{item.alt_country_image}</TableCell>
+                                <TableCell>{item.country_name}</TableCell>
+                                <TableCell>{item.country_address}</TableCell>
+                                <TableCell>
+                                    <div className="flex items-center justify-end gap-2">
+                                        <Button variant="secondary" size="icon" onClick={() => editModal(item)}>
+                                            <Edit2 />
+                                        </Button>
+                                        <Button variant="destructive" size="icon" onClick={() => deleteData(item._id)}>
+                                            <Trash2 />
+                                        </Button>
+                                    </div>
+
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
             </div>
         </div>
     )

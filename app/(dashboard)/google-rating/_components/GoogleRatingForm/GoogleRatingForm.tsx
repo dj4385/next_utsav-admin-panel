264a6d2827c017cfd6dd8a3b9f312ba330722639
dheeraj <dj4385@gmail.com>
2025-04-04 20:03:20 +0530

    google rating page done

--- app/(dashboard)/google-rating/_components/GoogleRatingForm/GoogleRatingForm.tsx
+++ app/(dashboard)/google-rating/_components/GoogleRatingForm/GoogleRatingForm.tsx
@@ -1,8 +1,104 @@
 'use client';
 
+import { ChangeEvent } from "react";
+
+import { useToast } from "@/hooks/use-toast";
+import { useAppDispatch } from "@/lib/store";
+import { useState } from "react";
+import { IAddAQIRequest } from "@/app/types/api/request/common.request";
+import { GoogleRatingService } from "@/services/google-rating.service";
+import { setAddGoogleRatingSuccess } from "@/lib/features/EventsSlice";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import ButtonComponent from "@/components/core/Button/Button";
+
 const GoogleRatingForm = () => {
+    const [range, setRange] = useState<string>('');
+    const [min, setMin] = useState<string>('');
+    const [max, setMax] = useState<string>('');
+    const [description, setDescription] = useState<string>('');
+    const [loading, setLoading] = useState<boolean>(false);
+
+    const { toast } = useToast();
+    const dispatch = useAppDispatch();
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const { name, value } = e.target;
+        if(name == 'range') {
+            setRange(value);
+        }
+        if(name == 'min') {
+            setMin(value);
+        }
+        if(name == 'max') {
+            setMax(value);
+        }
+        if(name == 'description') {
+            setDescription(value);
+        }
+    }
+
+    const onSave = async () => {
+        try {
+            if (!range || !min || !max || !description) {
+                return;
+            }   
+            setLoading(true);
+            const req: IAddAQIRequest = {
+                range: range,
+                min: parseInt(min),
+                max: parseInt(max),
+                description: description
+            }
+            const res: any = await GoogleRatingService.addGoogleRating(req);
+            if (res && (res.status == 200 || res.status == 201)) { 
+                toast({
+                    title: "Google Rating added successfully",
+                    description: "Google Rating added successfully",
+                })
+                dispatch(setAddGoogleRatingSuccess(true));
+                setRange('');
+                setMin('');
+                setMax('');
+                setDescription('');
+            } else {
+                toast({
+                    title: "Error",
+                    description: "Unable to add google rating",
+                    variant: "destructive",
+                })
+            }
+            setLoading(false);
+
+        } catch (error) {
+            console.log(error);
+            setLoading(false);
+        }
+    }
     return (
-        <></>
+        <div className="border-[2px] rounded-lg overflow-hidden w-full bg-white">
+            <div className="grid lg:grid-cols-2 grid-cols-1 gap-4 p-2">
+                <div>
+                    <label className="block text-sm font-medium text-gray-700">Range</label>
+                    <Input type="text" placeholder="Enter Location Name" className="mt-1 w-full" onChange={handleChange} name="range" value={range} />
+                </div>
+                <div>
+                    <label className="block text-sm font-medium text-gray-700">Min</label>
+                    <Input type="number" placeholder="Enter Min" className="mt-1 w-full" onChange={handleChange} name="min" value={min} />
+                </div>
+                <div>
+                    <label className="block text-sm font-medium text-gray-700">Max</label>
+                    <Input type="number" placeholder="Enter Max" className="mt-1 w-full" onChange={handleChange} name="max" value={max} />
+                </div>
+                <div>
+                    <label className="block text-sm font-medium text-gray-700">Description</label>
+                    <Textarea placeholder="Enter Description" className="mt-1 w-full" onChange={handleChange} name="description" value={description} />
+                </div>
+            </div>
+            <div className="flex justify-center items-center my-5">
+                <ButtonComponent label="Save Changes" onClick={() => onSave()} loading={loading} type="button" customClass="bg-purple-700 hover:bg-purple-800" />
+            </div>
+        </div>
     )
 }
 
--- app/(dashboard)/google-rating/_components/GoogleRatingTable/GoogleRatingTable.tsx
+++ app/(dashboard)/google-rating/_components/GoogleRatingTable/GoogleRatingTable.tsx
@@ -1,8 +1,101 @@
 'use client';
 
+import { TableCell } from "@/components/ui/table";
+
+import { TableBody } from "@/components/ui/table";
+
+import { useAppSelector } from "@/lib/store";
+
+import { useToast } from "@/hooks/use-toast";
+import { useAppDispatch } from "@/lib/store";
+import { GoogleRatingService } from "@/services/google-rating.service";
+import { useEffect, useState } from "react";
+import { Table, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { Button } from "@/components/ui/button";
+import { Trash2 } from "lucide-react";
+
 const GoogleRatingTable = () => {
+    const [googleRatingList, setGoogleRatingList] = useState<any[]>([])
+    const { toast } = useToast();
+    const dispatch = useAppDispatch();
+    const { isAddGoogleRatingSuccess } = useAppSelector((state) => state.EventsSlice);
+
+    const getCapacityList = async () => {
+        try {
+            const res: any = await GoogleRatingService.getGoogleRating();
+            if (res && res.status == 200 && res.data.data.length) {
+                setGoogleRatingList(res.data.data);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const deleteCapacity = async (id: string) => {
+        try {
+            const res: any = await GoogleRatingService.deleteGoogleRating(id);
+            if (res && res.status == 200) {
+                toast({
+                    title: "Capacity deleted successfully",
+                    description: "Capacity deleted successfully",
+                })
+                getCapacityList();
+            } else {
+                toast({
+                    title: "Error",
+                    description: "Unable to delete capacity",
+                    variant: "destructive",
+                })  
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        getCapacityList();
+    }, [])
+
+    useEffect(() => {
+        if(isAddGoogleRatingSuccess) {
+            getCapacityList();
+        }
+    }, [isAddGoogleRatingSuccess])
+
     return (
-        <></>
+        <div className="border-[2px] rounded-lg overflow-hidden w-full bg-white">
+            <div className="p-2">
+                <Table className="rounded-md">
+                    <TableHeader>
+                        <TableRow className="bg-purple-600 hover:bg-purple-600 text-white">
+                            <TableHead className="text-white">Range</TableHead>
+                            <TableHead className="text-white">Min</TableHead>
+                            <TableHead className="text-white">Max</TableHead>
+                            <TableHead className="text-white">Description</TableHead>
+                            <TableHead className="text-white text-right">Action</TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        {googleRatingList?.length ? googleRatingList.map((item, index) => (
+                            <TableRow key={index}>
+                                <TableCell>{item.range}</TableCell>
+                                <TableCell>{item.min}</TableCell>
+                                <TableCell>{item.max}</TableCell>
+                                <TableCell>{item.description}</TableCell>
+                                <TableCell>
+                                    <div className="flex items-center justify-end gap-2">
+                                        <Button variant="destructive" size="icon" onClick={() => deleteCapacity(item._id)}>
+                                            <Trash2 />
+                                        </Button>
+                                    </div>
+
+                                </TableCell>
+                            </TableRow>
+                        )) : null}
+                    </TableBody>
+                </Table>
+            </div>
+        </div>
     )
 }
 

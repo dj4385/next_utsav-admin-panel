b42ce16a266b1c241795b770a25705c01ba2c0a0
dheeraj <dj4385@gmail.com>
2025-04-05 17:31:11 +0530

    venue theme modal changes done

--- components/Modals/VenueThemeModal.tsx
+++ components/Modals/VenueThemeModal.tsx
@@ -10,19 +10,47 @@ import { Input } from "@/components/ui/input"
 import { useAppSelector } from "@/lib/store"
 import { useDispatch } from "react-redux"
 import { Textarea } from "../ui/textarea"
-import { useForm } from "react-hook-form"
+import { useFieldArray, useForm } from "react-hook-form"
 import ButtonComponent from "../core/Button/Button"
 import { useEffect, useState } from "react"
 import { IThemes } from "@/app/types/api/request/venue.request"
 import { setVenueThemeListItem, setVenueThemeModal, setVenueThemeModalList } from "@/lib/features/venue/VenueThemeSlice"
 import MultipleFileUploader from "../MultipleFileUploader/MultipleFileUploader"
+import { Plus, Trash2 } from "lucide-react"
+import { Button } from "../ui/button"
+import { Checkbox } from "../ui/checkbox"
+import FileUploader from "../FileUploader/FileUploader"
+
+type FormValues = {
+    name: string;
+    tab_name: string;
+    description: string;
+    theme_img_gallery: {
+        alt: string;
+        images: string;
+        is_wide: boolean;
+    }[];
+};
+
 
 const VenueThemeModal = () => {
     const [loading, setLoading] = useState<boolean>(false);
     const { isOpen, themeListItem } = useAppSelector((state) => state.VenueThemeSlice);
     const dispatch = useDispatch();
-    const { register, handleSubmit, reset, setValue, formState: { errors } } = useForm<any>();
+    const { register, handleSubmit, reset, setValue, formState: { errors }, control, watch } = useForm<FormValues>({
+        defaultValues: {
+            name: '',
+            tab_name: '',
+            description: '',
+            theme_img_gallery: [{ alt: '', images: '', is_wide: false }],
+        }
+    });
     const [imageUrl, setImageUrl] = useState<string[]>([]);
+
+    const { fields, append, remove } = useFieldArray({
+        control,
+        name: 'theme_img_gallery',
+    });
 
     const onSubmit = (data: any) => {
         setLoading(true);
@@ -71,17 +99,13 @@ const VenueThemeModal = () => {
 
     return (
         <Dialog open={isOpen} onOpenChange={handleClose}>
-            <DialogContent className="w-[50%]" aria-describedby="Event Modal">
+            <DialogContent className="w-[90%] max-h-[80vh] overflow-y-auto" aria-describedby="Event Modal">
                 <DialogHeader>
                     <DialogTitle>Add/Update Venue Theme</DialogTitle>
                 </DialogHeader>
                 <div className="grid gap-4 py-4 overflow-y-auto">
                     <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
                         <div className="grid lg:grid-cols-1 grid-cols-1 gap-4 p-2">
-                            <div>
-                                <label className="block text-sm font-medium text-gray-700 mb-2">Images</label>
-                                <MultipleFileUploader url={themeListItem?.theme_img_gallery?.map((gallery) => gallery.images) || []} urlType="image" onFileUpload={(url: string[]) => onImageUpload(url)} />
-                            </div>
                             <div>
                                 <label className="block text-sm font-medium text-gray-700">Name</label>
                                 <Input type="text" {...register("name")} placeholder="Enter Theme Name" className="mt-1 w-full" />
@@ -93,6 +117,51 @@ const VenueThemeModal = () => {
                             <div>
                                 <label className="block text-sm font-medium text-gray-700">Description</label>
                                 <Textarea {...register("description")} placeholder="Enter Description" className="mt-1 w-full" />
+                            </div>
+                            <div className="space-y-2">
+                                <h3 className="font-bold">Theme Image Gallery</h3>
+                                {fields.map((field, index) => (
+                                    <div key={field.id} className="grid lg:grid-cols-1 grid-cols-1 gap-4 p-2 border-2 rounded-lg">
+                                        <FileUploader
+                                            onFileUpload={(url: string) => setValue(`theme_img_gallery.${index}.images`, url)}
+                                            url={watch(`theme_img_gallery.${index}.images`)}
+                                            urlType="image"
+                                        />
+                                        <Input
+                                            {...register(`theme_img_gallery.${index}.alt` as const)}
+                                            placeholder="Alt text"
+                                            className="border p-2 w-full"
+                                        />
+                                        <label className="flex items-center space-x-2">
+                                            <Checkbox 
+                                                {...register(`theme_img_gallery.${index}.is_wide` as const)}
+                                                checked={watch(`theme_img_gallery.${index}.is_wide`)}
+                                                onCheckedChange={(checked: boolean) => {
+                                                    // Uncheck all other checkboxes
+                                                    fields.forEach((_, i) => {
+                                                        if (i !== index) {
+                                                            setValue(`theme_img_gallery.${i}.is_wide`, false);
+                                                        }
+                                                    });
+                                                    // Set the current checkbox
+                                                    setValue(`theme_img_gallery.${index}.is_wide`, checked);
+                                                }}
+                                            />
+                                            <span>Is Wide</span>
+                                        </label>
+                                        <div className="flex justify-end">
+                                            <Button type="button" variant="destructive" size="icon" onClick={() => remove(index)}>
+                                                <Trash2 />
+                                            </Button>
+                                        </div>
+
+                                    </div>
+                                ))}
+                                <div className="flex justify-end">
+                                    <Button type="button" variant="secondary" size="icon" onClick={() => append({ alt: '', images: '', is_wide: false })}>
+                                        <Plus />
+                                    </Button>
+                                </div>
                             </div>
                             <ButtonComponent label="Save Changes" onClick={() => { }} loading={loading} type="submit" customClass="w-full bg-purple-700 hover:bg-purple-800" />
                         </div>

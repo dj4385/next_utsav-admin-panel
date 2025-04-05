6173084529df39b13517949ed4174195b1900d2f
dheeraj <dj4385@gmail.com>
2025-04-05 18:55:02 +0530

    venue update done

--- app/(dashboard)/venue/update-venue/[id]/_components/UpdateVenueForm.tsx
+++ app/(dashboard)/venue/update-venue/[id]/_components/UpdateVenueForm.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { IThemes } from "@/app/types/api/request/venue.request";
+import { IThemes, IHeaderImages, IAddVenueRequest } from "@/app/types/api/request/venue.request";
 import ContentHeader from "@/components/ContentHeader/ContentHeader";
 import { ArrowLeft, MapPinHouse } from "lucide-react";
 import { useEffect, useState } from "react";
@@ -12,6 +12,8 @@ import ThemeForm from "../../../add-venue/_components/ThemeForm/ThemeForm";
 import VenuesImages from "../../../add-venue/_components/VenueImages/VenuesImages";
 import { VenueService } from "@/services/venue.service";
 import NearestAirport from "../../../add-venue/_components/NearestAirport/NearestAirport";
+import HeaderImage from "../../../add-venue/_components/HeaderImage/HeaderImage";
+import { toast } from "@/hooks/use-toast";
 
 const UpdateVenueForm = ({ id }: { id: string }) => {
     const [venueApiResponse, setVenueApiResponse] = useState<any>();
@@ -20,6 +22,7 @@ const UpdateVenueForm = ({ id }: { id: string }) => {
     // const [realWeddings, setRealWeddings] = useState<IRealWeddings[]>([]);
     const [themeFormList, setThemeFormList] = useState<IThemes[]>([]);
     const [venueImagesList, setVenueImages] = useState<IImages[]>([]);
+    const [headerImageList, setHeaderImageList] = useState<IHeaderImages[]>([]);
     const [loading, setLoading] = useState<boolean>(false);
 
     const router = useRouter();
@@ -41,18 +44,75 @@ const UpdateVenueForm = ({ id }: { id: string }) => {
                     experience: res.data.experience,
                     location: res.data.location,
                     theme_options_heading: res.data.theme_options_heading,
-                    theme_options_description: res.data.theme_options_description,
+                    theme_options_subheading: res.data.theme_options_subheading,
                 });
                 setThemeFormList(res.data.theme_options);
                 setVenueImages(res.data.gallery);
                 setNearestAirport(res.data.venue.nearest_airport);
+                setHeaderImageList(res.data.header_image_gallery);
             }
         } catch (error) {
             
         }
     }
 
-    const onSaveChanges = () => {}
+    const onSaveChanges = async () => {
+        try {
+            setLoading(true);
+            debugger
+            const req: IAddVenueRequest = {
+                venue_name: venueData?.venue_name,
+                venue: {
+                    "property_type": venueData?.property_type,
+                    "capacity": +venueData?.capacity,
+                    "nearest_airport": {
+                        "name": nearestAirport?.name,
+                        "distance_km": +nearestAirport?.distance_km
+                    },
+                    "outdoor_catering_policy": venueData?.outdoor_catering_policy,
+                    "air_quality_index": +venueData?.air_quality_index,
+                    "google_rating": +venueData?.google_rating,
+                    "google_rating_review": +venueData?.google_rating_review,
+                    "description": venueData?.description,
+                    "map_link": venueData?.map_link
+                },
+                theme_options_heading: venueData?.theme_options_heading,
+                theme_options_subheading: venueData?.theme_options_subheading,
+                theme_options: themeFormList,
+                gallery: venueImagesList,
+                location: venueData?.location?._id,
+                experience: venueData?.experience?._id,
+                header_image_gallery: headerImageList,
+            }
+
+            console.log(req, 'req');
+
+
+            const res: any = await VenueService.updateVenue(id, req);
+
+            if (res?.status == 200 || res?.status == 201) {
+                toast({
+                    title: "Venue Updated Successfully",
+                    description: res.message
+                })
+                back();
+            } else {
+                toast({
+                    title: "Venue Updated Failed",
+                    description: res?.response?.data?.error,
+                    variant: "destructive"
+                })
+            }
+        } catch (error: any) {
+            toast({
+                title: "Error",
+                description: error.message,
+                variant: "destructive"
+            })
+        } finally {
+            setLoading(false);
+        }
+    }
 
     useEffect(() => {
         if (id) {
@@ -79,6 +139,9 @@ const UpdateVenueForm = ({ id }: { id: string }) => {
             </div>
             <div className="flex flex-row mt-5">
                 <VenuesImages setVenueImages={setVenueImages} venueImagesList={venueImagesList} />
+            </div>
+            <div className="flex flex-row mt-5">
+                <HeaderImage setHeaderImageList={setHeaderImageList} headerImageList={headerImageList} />
             </div>
             <div className="flex justify-center items-center mt-5">
                 <ButtonComponent label="Save Changes" onClick={() => onSaveChanges()} loading={loading} type="button" customClass="bg-purple-700 hover:bg-purple-800" />
